984e69275f6e8ad4b1a0577748bc00b6784fe1b5
Ben Butmir <39421227+Mexmorize@users.noreply.github.com>
2023-01-19 14:23:58 -0500

    feat(FrontEnd): added new chakra tsx frontend (#45)

--- frontend/src/_mock/account.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------
-
-const account = {
-  displayName: 'Benjamin Butmir',
-  email: 'ben.butmir@dcp.com',
-  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
-};
-
-export default account;
